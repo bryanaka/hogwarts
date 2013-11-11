81e8c5b6bd74dae3c4147a1509aba4c6e86a292d
Bryan Robles <cyborboyx@gmail.com>
2013-11-10 23:36:48 -0800

    almost passing the house controller show action tests.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Hogwarts::Application.routes.draw do
   # screwing up unit tests, deal with in a bit
   # route to: 'welcome/index'
 
-  get '/houses'   => 'houses#index'
+  get '/houses'     => 'houses#index'
+  get '/houses/:id' => 'houses#show'
+
   get '/students' => 'students#index'
   get '/students' => 'students#creat'
 end
