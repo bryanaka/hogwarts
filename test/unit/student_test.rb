d6d187f1feb77359a1989a2802781eaccf497717
Bryan Robles <cyborboyx@gmail.com>
2013-11-10 22:51:57 -0800

    wrote tests for student model

--- test/unit/student_test.rb
+++ test/unit/student_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
+
+  def setup
+    @harry = Student.new(:name => "Harry Potter")
+  end
+
+  test "has a name attribute" do
+    assert_equal @harry.name, "Harry Potter"
+  end
+
+  test "is invalid without a name" do
+    ron = Student.new
+    assert(!ron.valid?)
+  end
+
 end
